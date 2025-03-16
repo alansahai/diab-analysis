6c7c79623d560895bb9de012435a5e97c277b46f
alansahai <alansahai123@gmail.com>
2025-03-16 22:57:33 +0530

    Initial commit

---
+++ Diabetes_Analysis.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 15:00:46 2024
+
+@author: alans
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+loaded_model = pickle.load(open('./training_model.sav', 'rb'))
+
+def diabetes_prediction(input_data):
+    input_data_as_numpy_array = np.asarray(input_data)
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    prediction = loaded_model.predict(input_data_reshaped)
+    print(prediction)
+
+    if (prediction[0] == 0):
+      return 'The person is not diabetic'
+    else:
+      return 'The person is diabetic'
+  
+def main():
+    st.title('Diabetes Predicting Web App')
+    
+    Pregnancies = st.text_input('Number of Pregnancies')
+    Glucose = st.text_input('Glucose Level')
+    BloodPressure = st.text_input('Blood Pressure Value')
+    SkinThickness = st.text_input('Skin Thickness Value')
+    Insulin = st.text_input('Insulin Value')
+    BMI = st.text_input('BMI Value')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function Value')
+    Age = st.text_input('Age of the Person')
+    
+    diagnosis = ''
+    
+    if st.button('Diabetes Test Result'):
+        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+        
+    st.success(diagnosis)
+    
+if __name__ == '__main__':
+    main()
